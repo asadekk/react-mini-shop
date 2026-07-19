4281744b0f67e9892666b1b9b1df05bc7f9123ac
asadekk <asadesd21@gmail.com>
2026-07-19 18:40:42 +0500

    - + and total price

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -6,11 +6,41 @@ export function CartProvider({ children }) {
   const [cart, setCart] = useState([]);
 
   const addToCart = (product) => {
-    setCart((prev) => [...prev, product]);
-  };
+    const count = cart.find((item) => item.id === product.id);
+    if (count) {
+      setCart(cart.map((item) => {
+        return item.id === product.id ?
+          { ...item, quantity: item.quantity + 1 } :
+          item;
+      }))
+    } else {
+      setCart([...cart, { ...product, quantity: 1 }])
+    }
+  }
+
+  const pilusCart = (id) => {
+    setCart(cart.map((item) => {
+      return item.id === id ?
+        { ...item, quantity: item.quantity + 1 } :
+        item
+    }))
+  }
+
+  const minusCart = (id) => {
+    setCart(cart.map((item) => {
+      return item.id === id ?
+        {
+          ...item, quantity: item.quantity > 1 ? item.quantity - 1 : 1
+        } : item
+    }))
+  }
+
+  const deleteCart = (id) => {
+    setCart(cart.filter((item)=> item.id !== id))
+  }
 
   return (
-    <CartContext.Provider value={{ cart, addToCart }}>
+    <CartContext.Provider value={{ cart, addToCart, pilusCart, minusCart, deleteCart }}>
       {children}
     </CartContext.Provider>
   );
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,29 +2,39 @@ import React from "react";
 import { useCart } from "../context/CartContext";
 import '../css/Cart.css'
 function Cart() {
-  const { cart } = useCart();
+  const { cart, pilusCart, minusCart, deleteCart } = useCart();
 
   return (
-   <div className="cart-container">
-  {cart.length === 0 ? (
-    <h2 className="empty-cart">Savat bo'sh</h2>
-  ) : (
-    cart.map((product) => (
-      <div className="cart-item" key={product.id}>
-        <img src={product.thumbnail} alt={product.title} />
+    <div className="cart-container">
+      <h2>
+        Total: $
+        {cart.reduce(
+          (sum, item) => sum + item.price * item.quantity,
+          0
+        )}
+      </h2>
+      {cart.length === 0 ? (
+        <h2 className="empty-cart">Savat bo'sh</h2>
+      ) : (
+        cart.map((product) => (
+          <div className="cart-item" key={product.id}>
+            <img src={product.thumbnail} alt={product.title} />
 
-        <div className="cart-info">
-          <h3>{product.title}</h3>
-          <p>${product.price}</p>
-        </div>
+            <div className="cart-info">
+              <h3>{product.title}</h3>
+              <p>${product.price}</p>
+            </div>
+            <span>{product.quantity}</span>
+            <div>
+              <button onClick={() => pilusCart(product.id)}>+</button>
+              <button onClick={() => minusCart(product.id)}>-</button>
 
-        {/* <button className="cart-remove">
-          O'chirish
-        </button> */}
-      </div>
-    ))
-  )}
-</div>
+            </div>
+            <button onClick={() => deleteCart(product.id)}>DELETE</button>
+          </div>
+        ))
+      )}
+    </div>
   );
 }
 
